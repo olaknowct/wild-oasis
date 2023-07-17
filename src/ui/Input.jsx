7a4d2597f1b367d37421ca2e06a049d16db4dc62
Christopher Olano <olanochristopher@gmail.com>
2023-07-17 20:39:47 +0800

    Input reusable styled component

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -3,10 +3,8 @@ import styled from 'styled-components';
 const Input = styled.input`
   border: 1px solid var(--color-grey-300);
   background-color: var(--color-grey-0);
-  border-radius: var(--border-raidus-sm);
+  border-radius: var(--border-radius-sm);
   padding: 0.8rem 1.2rem;
-  display: block;
-  margin-top: 2rem;
   box-shadow: var(--shadow-sm);
 `;
 
