8fdfc88f75a999e3dc7985ef371466444f8d6d27
Christopher Olano <olanochristopher@gmail.com>
2023-07-21 19:50:30 +0800

    uploading images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ugxzdkbxclwbuqvbmobw.supabase.co';
+export const supabaseUrl = 'https://ugxzdkbxclwbuqvbmobw.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVneHpka2J4Y2x3YnVxdmJtb2J3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYwODM1NzMsImV4cCI6MTk5MTY1OTU3M30.FoqK3lJopqmLeFCYzKjoRNw-GzfvqOYbP8L8vZkzbck';
 const supabase = createClient(supabaseUrl, supabaseKey);
